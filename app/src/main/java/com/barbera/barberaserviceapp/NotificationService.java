f4087a556ed093188a8884358b468d88289817e8
Kush223 <kush_1901me37@iitp.ac.in>
2021-07-15 17:04:20 +0530

    notif

---
+++ app/src/main/java/com/barbera/barberaserviceapp/NotificationService.java
@@ -0,0 +1,46 @@
+package com.barbera.barberaserviceapp;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.SharedPreferences;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class NotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        String x=remoteMessage.getNotification().getBody();
+        String y=x.substring(0,6);
+        SharedPreferences sharedPreferences=getSharedPreferences("Notification",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("notif",y);
+        editor.apply();
+        sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+    private void sendNotification(String title,String message){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel=new NotificationChannel("Barbera","Barbera", NotificationManager.IMPORTANCE_DEFAULT);
+
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"Barbera")
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle(title)
+                .setAutoCancel(true)
+                .setContentText(message);
+
+        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(999,builder.build());
+
+    }
+
+}
